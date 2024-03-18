5170a081e3ba721d9edbf679892a2a055725fc92
Gabriel Medeiros <gabriel.medeiros@liferay.com>
2024-03-17 22:27:12 -0300

    init project

---
+++ src/main/java/dev/gabriel/domaindrivendesigndemo/DomainDrivenDesignDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.gabriel.domaindrivendesigndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDesignDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainDrivenDesignDemoApplication.class, args);
+    }
+
+}
